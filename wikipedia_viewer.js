c29157307471599840455ccef2bcaa871758edd3
Sophie Helf <sophie.helf@gmail.com>
2016-12-13 14:20:21 -0800

    Round 2

---
+++ wikipedia_viewer.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+
+  var bloop = true;
+  $('form').on('keydown', '.submit', function(e) {
+    
+    // action when return key pressed
+    
+    if (e.keyCode === 13) {
+      e.preventDefault();
+      var searchQuery = $(this).val();
+      $('.results').empty();
+      
+       //animate bubble
+      
+      if (bloop) {
+       $('.bubble').animate({width:'54%'}, 1500);
+       bloop = false;
+      }
+      
+      // json time   
+      
+      $.getJSON('https://en.wikipedia.org/w/api.php?action=opensearch&search=' + searchQuery + '&origin=*&format=json', function(data) {
+        
+        // loop thru data and push to results div 
+        
+        for (var i = 0; i <= data.length; i++) {
+          var sentence = data[2][i];
+         // if (sentence.indexOf("may refer to:") !== -1) {
+            
+         // }
+          
+           // sentence.indexOf(":") !== -1 
+          // sentence.pop 
+          // sentence + ... ???
+        $('<p> <a href=' + data[3][i] +'>' + data[1][i] + '</a>' + '<br>•<br>' + sentence + '<br></p>').appendTo('.results').hide().fadeIn(1500);
+        }
+      })
+      return false;
+    }
+ });
+  
+})
